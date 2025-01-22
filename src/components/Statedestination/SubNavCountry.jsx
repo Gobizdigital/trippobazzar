463590173a2a489e8e12d23752eefe32fe8a3b01
DesaiPriyansh19 <priyanshdesai62@gmail.com>
2025-01-22 15:35:58 +0530

    filter icon in state packages page

--- src/components/Statedestination/SubNavCountry.jsx
+++ src/components/Statedestination/SubNavCountry.jsx
@@ -6,7 +6,11 @@ function SubNavCountry({ toggleModal }) {
   return (
     <nav className="bg-white mt-[45rem] sm:mt-[40rem] md:mt-[26rem] lg:mt-60 sticky top-[80px] z-30 shadow-inner">
       <div className="max-w-7xl mx-auto px-4 py-4">
-        <ul className="flex flex-row em:flex-row  scrollbar-hide px-4 overflow-x-scroll items-center justify-start em:justify-center text-[.7rem] em:text-sm uppercase leading-6 font-normal  gap-8">
+        <ul className="flex flex-row em:flex-row  scrollbar-hide px-4 overflow-x-scroll items-center justify-start
+         em:justify-center text-[.7rem] em:text-sm uppercase leading-6 font-normal  gap-8">
+            <div className="cursor-pointer block md:absolute  md:left-32 mmd:left-48" onClick={toggleModal}>
+            <SmallerFilterSvg />
+          </div>
           {sections.map((item, idx) => (
             <button
               key={idx}
@@ -15,9 +19,7 @@ function SubNavCountry({ toggleModal }) {
               {item}
             </button>
           ))}
-          <div className="cursor-pointer block md:absolute  md:right-32 mmd:right-48" onClick={toggleModal}>
-            <SmallerFilterSvg />
-          </div>
+        
         </ul>
       </div>
     </nav>
