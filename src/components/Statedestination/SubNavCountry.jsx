4c2e5261e8f0129fcb847839220f86f67b660d9c
GoBizDigital <gobizzdigital@gmail.com>
2025-05-03 14:39:45 +0530

    Update SubNavCountry.jsx

--- src/components/Statedestination/SubNavCountry.jsx
+++ src/components/Statedestination/SubNavCountry.jsx
@@ -6,9 +6,14 @@ function SubNavCountry({ toggleModal }) {
   return (
     <nav className="bg-white mt-[30rem] sm:mt-[35rem] md:mt-[26rem] lg:mt-60 sticky top-[80px] z-30 shadow-inner">
       <div className="max-w-7xl mx-auto px-4 py-4">
-        <ul className="flex flex-row em:flex-row  scrollbar-hide px-4 overflow-x-scroll items-center justify-start
-         em:justify-center text-[.7rem] em:text-sm uppercase leading-6 font-normal  gap-8">
-            <div className="cursor-pointer block md:absolute  md:left-32 mmd:left-48" onClick={toggleModal}>
+        <ul
+          className="flex flex-row em:flex-row  scrollbar-hide px-4 overflow-x-scroll items-center justify-start
+         em:justify-center text-[.7rem] em:text-sm uppercase leading-6 font-normal  gap-8"
+        >
+          <div
+            className="cursor-pointer block md:absolute  md:left-32 mmd:left-48"
+            onClick={toggleModal}
+          >
             <SmallerFilterSvg />
           </div>
           {sections.map((item, idx) => (
@@ -19,7 +24,6 @@ function SubNavCountry({ toggleModal }) {
               {item}
             </button>
           ))}
-        
         </ul>
       </div>
     </nav>
